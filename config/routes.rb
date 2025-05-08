9cfe528b7c7ff832034c657476072007998adcdf
Daniel Febles <daniel.febles97@gmail.com>
2025-05-08 19:16:14 -0400

    Unsubscribing via email works now

--- config/routes.rb
+++ config/routes.rb
@@ -6,19 +6,10 @@ Rails.application.routes.draw do
   resources :products do # This does the same the get/post/put/patch/delete stuff commented out below
     resources :subscribers, only: [ :create ]
   end
+
+  resource :unsubscribe, only: [ :show ]
+
   root "products#index"
-  #   get "/products", to: "products#index"
-
-  #   get "/products/new", to: "products#new"
-  #   post "/products", to: "products#create"
-
-  #   get "/products/:id", to: "products#show"
-
-  #   get "/products/:id/edit", to "products#edit"
-  #   put "/products/:id", to "products#update"
-  #   patch "/products/:id", to "products#update"
-
-  #   delete "/products/:id", to: "products#destroy"
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
