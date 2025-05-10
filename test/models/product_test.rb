cf243a82286a0e345de46755c82dff57bf9a36a6
Daniel Febles <daniel.febles97@gmail.com>
2025-05-10 18:01:17 -0400

    Add tests

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,17 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  # This will help us monitor emails sent during the test
+  # Basically imports assert_emails
+  include ActionMailer::TestHelper
+  test "sends email notifications when back in stock" do
+    product = products(:tshirt)
+
+    product.update(inventory_count: 0)
+
+    # Essentially, this asserts 2 emails are sent when the callback is run
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
